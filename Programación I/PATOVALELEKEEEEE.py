714c793484254ba5622f7a825ddb0f0e966887f9
RicardoMaldonado14 <ricardohmaldonado06@gmail.com>
2026-04-01 17:17:10 -0300

    Delete Programación I/PATOVALELEKEEEEE.py

--- Programación I/PATOVALELEKEEEEE.py
+++
@@ -1,9 +0,0 @@
-documento=bool(input("trajiste documento? SI/NO ")=="SI")
-if documento==True:
-    edad=int(input("edad del wachin: "))
-    if edad>=18 and documento==True:
-        print("pasa pibe")
-    else:
-        print("no podes pasar")
-else:
-    print("no podes pasar")
